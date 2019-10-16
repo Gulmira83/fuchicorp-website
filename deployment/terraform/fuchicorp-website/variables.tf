d95403f0f43cffc6614f8815df04d6ec31090e3a
LeilaDev <leiladevops@gmail.com>
2019-10-16 16:03:32 -0500

    variables tf added

---
+++ deployment/terraform/fuchicorp-website/variables.tf
@@ -0,0 +1,19 @@
+  
+variable "namespace" {
+  default = "test"
+}
+variable "version" {
+  default = "6.0.1"
+}
+variable "name" {
+  default = "fuchicorp-website"
+}
+variable "domain_name" {
+  default = "fuchicorp.com"
+}
+variable "docker_image" {
+  default = "docker.fuchicorp.com/main-website-dev"
+}
+variable "docker_image_tag" {
+  default = "latest"
+}
