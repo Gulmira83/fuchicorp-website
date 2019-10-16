677266502e64635ce11e58e1c45049b2fb90128a
LeilaDev <leiladevops@gmail.com>
2019-10-16 15:44:58 -0500

    added values to website.tf

---
+++ deployment/terraform/fuchicorp-website/website.tf
@@ -0,0 +1,24 @@
+data "template_file" "fuchicorp-website_values" {
+  template = "${file("./fuchicorp-website/template_values.yaml")}"
+  vars = {
+    domain_name = "${var.domain_name}"
+    docker_image = "${var.docker_image}"
+    docker_image_tag = "${var.docker_image_tag}"
+  }
+}
+
+resource "local_file" "fuchicorp-website_values_local_file" {
+  content  = "${trimspace(data.template_file.fuchicorp-website_values.rendered)}"
+  filename = "./fuchicorp-website/.cache/values.yaml"
+}
+
+
+resource "helm_release" "fuchicorp-website" {
+  name       = "${var.name}"
+  namespace = "${var.namespace}"
+  chart = "./fuchicorp-website"
+  version    = "${var.version}"
+   values = [
+    "${data.template_file.fuchicorp-website_values.rendered}"
+  ]
+}
